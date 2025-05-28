6a74063fa16eadda53cd92b5b2832b3117650d66
RaZel713 <razel7130@gmail.com>
2025-05-28 19:55:59 +0900

    test: 변경된 포트 설정으로 예약 컨트롤러 테스트 환경 수정

--- src/test/java/roomescape/presentation/rest/ReservationControllerTest.java
+++ src/test/java/roomescape/presentation/rest/ReservationControllerTest.java
@@ -6,12 +6,15 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.util.Map;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
@@ -22,14 +25,23 @@ import roomescape.exception.PaymentException;
 import roomescape.infrastructure.PaymentClient;
 
 @ActiveProfiles("test")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ReservationControllerTest {
 
     private static final Map<String, String> RESERVATION_BODY = Map.of("date", "3000-03-17", "timeId", "1", "themeId",
             "1", "memberId", "2", "paymentKey", "paymentKey", "orderId", "orderId", "amount", "1000");
+
+    @LocalServerPort
+    int port;
+    
     @MockBean
     private PaymentClient paymentClient;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
 
     @Test
     @DisplayName("예약 추가 요청시, id를 포함한 예약 내용과 CREATED를 응답한다")
