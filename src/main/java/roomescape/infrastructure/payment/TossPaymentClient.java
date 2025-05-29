d9b74c43fe2dfc41a31dda49dd356c68359d0ac6
junhaa <2171326@hansung.ac.kr>
2025-05-29 13:45:18 +0900

    refactor: timeout 설정을 프로퍼티 파일로 분리

--- src/main/java/roomescape/infrastructure/payment/TossPaymentClient.java
+++ src/main/java/roomescape/infrastructure/payment/TossPaymentClient.java
@@ -6,13 +6,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import roomescape.application.request.PaymentInfo;
+import roomescape.application.response.PaymentResponse;
 import roomescape.application.response.TossErrorResponse;
-import roomescape.domain.payment.Payment;
 import roomescape.exception.PaymentException;
 
 @Component
@@ -22,17 +20,12 @@ public class TossPaymentClient implements PaymentClient {
     private static final String TOSS_PAYMENT_CONFIRM_URI = "/v1/payments/confirm";
     private static final String SECRET_KEY_PREFIX = "Basic ";
     private static final String DELIMITER = ":";
-    private static final int CONNECT_TIMEOUT = 3000;
-    private static final int READ_TIMEOUT = 10000;
 
-    private final ObjectMapper objectMapper;
     private final RestClient restClient;
 
-    public TossPaymentClient(
-            @Value("${payment.secret-key.toss}") String secretKey, final ObjectMapper objectMapper
-    ) {
-        this.objectMapper = objectMapper;
-        this.restClient = getRestClient(secretKey);
+    public TossPaymentClient(@Value("${payment.secret-key.toss}") String secretKey, ObjectMapper objectMapper,
+                             RestClient.Builder restClientBuilder) {
+        this.restClient = getRestClient(restClientBuilder, objectMapper, secretKey);
     }
 
     public PaymentResponse confirmPayment(PaymentInfo paymentInfo) {
@@ -40,30 +33,17 @@ public class TossPaymentClient implements PaymentClient {
                 .retrieve().body(PaymentResponse.class);
     }
 
-    private RestClient getRestClient(String secretKey) {
+    private RestClient getRestClient(RestClient.Builder builder, ObjectMapper objectMapper, String secretKey) {
         String encodedKey = getEncodedKey(secretKey + DELIMITER);
 
-        return RestClient.builder()
-                .requestFactory(createFactory())
-                .baseUrl(TOSS_BASE_URL)
-                .defaultStatusHandler(
-                        status -> status.is4xxClientError() || status.is5xxServerError(),
+        return builder.baseUrl(TOSS_BASE_URL)
+                .defaultStatusHandler(status -> status.is4xxClientError() || status.is5xxServerError(),
                         (request, response) -> {
                             TossErrorResponse error = objectMapper.readValue(response.getBody(),
                                     TossErrorResponse.class);
                             PaymentErrorCode code = PaymentErrorCode.from(error.code());
                             throw new PaymentException(code);
-                        }
-                )
-                .defaultHeader(AUTHORIZATION, SECRET_KEY_PREFIX + encodedKey)
-                .build();
-    }
-
-    private ClientHttpRequestFactory createFactory(){
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout(CONNECT_TIMEOUT);
-        factory.setReadTimeout(READ_TIMEOUT);
-        return factory;
+                        }).defaultHeader(AUTHORIZATION, SECRET_KEY_PREFIX + encodedKey).build();
     }
 
     private String getEncodedKey(String key) {
